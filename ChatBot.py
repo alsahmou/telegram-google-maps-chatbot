7c6af8d858503b9d2e9e57cfa68b9eaffb2b6a53
Ali Sahmoud <ali.sahmoud@mail.mcgill.ca>
2019-11-19 01:19:26 -0800

    Resizing image README

--- ChatBot.py
+++ ChatBot.py
@@ -12,13 +12,13 @@ import sys
 from datetime import datetime
 from configparser import ConfigParser
 
-#Reading key and token
+# Reading key and token
 with open ('config.json') as f:
     input_keys = json.loads(f.read())
     TOKEN = input_keys['auth']['Bot_Token']
     API_KEY = input_keys['auth']['Google_API_Key']
 
-#List of places types that a user can input
+# List of places types that a user can input
 places_types = ['accounting', 'airport', 'amusement_park',
     'aquarium', 'art_gallery', 'atm', 'bakery', 'bank', 'bar', 
     'beauty_salon', 'bicycle_store', 'book_store', 'bowling_alley', 
@@ -37,21 +37,21 @@ places_types = ['accounting', 'airport', 'amusement_park',
     'veterinary_care', 'zoo']
 
 
-#Constants and initializing variables
+# Constants and initializing variables
 FILE_NAME = "StoredRatings.txt"
 chat_id = 0
 gmaps = googlemaps.Client(key=API_KEY)
 
-#Reads data on the drive
+# Reads data on the drive
 def read_ratings(file_name):
-    if not os.path.isfile(FILE_NAME): #If the file doesn't exist
+    if not os.path.isfile(FILE_NAME): # If the file doesn't exist
         stored_ratings = {}
         return stored_ratings
-    with open (FILE_NAME) as f: #If the file exists 
+    with open (FILE_NAME) as f: # If the file exists 
         stored_ratings = json.loads(f.read())
         return stored_ratings
 
-#Saves new data on the drive
+# Saves new data on the drive
 def write_ratings(stored_ratings): 
     file = open(FILE_NAME, "w")
     encoded_dict = json.dumps(stored_ratings)
@@ -63,14 +63,14 @@ class MessageCounter(telepot.helper.ChatHandler):
         super(MessageCounter, self).__init__(*args, **kwargs)
         self.state = 'prompt for type'
 
-    #Prompts user for the location type:
+    # Prompts user for the location type:
     def prompt_for_type(self, msg):
         self.sender.sendMessage('What kind of location are you looking for? Enter types of locations such as (restaurant, gas_station, pharmacy),\
         if the types has more than 1 word, please enter it using an underscore such as (gas_station)')
         self.state = 'type received'
         return self.state
 
-    #Prompts user for the origin's address
+    # Prompts user for the origin's address
     def prompt_for_address(self, msg):
         if msg['text'].lower() in places_types:
             self.location_type = msg['text'].lower()
@@ -81,7 +81,7 @@ class MessageCounter(telepot.helper.ChatHandler):
             self.state = 'type received'
         return self.state
 
-    #Prompts user for the maximum radius
+    # Prompts user for the maximum radius
     def prompt_for_radius(self, msg):
         self.users_address_longitude = msg['location']['longitude']
         self.users_address_latitude = msg['location']['latitude']
@@ -91,7 +91,7 @@ class MessageCounter(telepot.helper.ChatHandler):
         self.state = 'radius received'
         return self.state
 
-    #Prompts user for their rating of the location
+    # Prompts user for their rating of the location
     def prompt_for_rating(self, msg):
         stripped_radius = msg['text'].strip()
         if str.isnumeric(stripped_radius) == True:
@@ -106,7 +106,7 @@ class MessageCounter(telepot.helper.ChatHandler):
             self.sender.sendMessage('Invalid radius, please try again')
             return self.state
 
-    #Formats the user's rating to a dictionary to be stored on the drive later
+    # Formats the user's rating to a dictionary to be stored on the drive later
     def store_rating(self, stored_ratings, location_id, user_rating):
         if chat_id not in stored_ratings:
             stored_ratings[chat_id] = {}
@@ -116,7 +116,7 @@ class MessageCounter(telepot.helper.ChatHandler):
         user_stored_ratings[location_id].append(user_rating)
         write_ratings(stored_ratings)
 
-    #Stores the user's rating  on the drive
+    # Stores the user's rating  on the drive
     def store_check_ratings(self, msg):
         stripped_rating = msg['text'].strip()
         if str.isnumeric(stripped_rating) == True and int(msg['text']) <= 5 and int(msg['text']) >1:
@@ -131,12 +131,12 @@ class MessageCounter(telepot.helper.ChatHandler):
             self.sender.sendMessage('Invalid rating, please try again')
         return self.state
     
-    #Terminates the bot
+    # Terminates the bot
     def terminate_bot(self, msg):
         self.sender.sendMessage('Thanks for using the bot. To reuse the bot please type /start. Have a good day!')
         return 
 
-    #Handles the states
+    # Handles the states
     def on_chat_message(self, msg):
         if 'text' in msg and msg['text'] == '/stop':
             self.terminate_bot(msg)
